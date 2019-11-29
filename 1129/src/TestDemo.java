4c230b6e5675b6167b99d2c2641d445f858edd03
qingzibbb <981232469@qq.com>
2019-11-29 19:50:05 +0800

    双链表

---
+++ 1129/src/TestDemo.java
@@ -0,0 +1,19 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        DoubleList doubleList = new DoubleList();
+        doubleList.addFirst(1);
+        doubleList.addLast(2);
+        doubleList.addLast(2);
+        doubleList.addLast(2);
+        doubleList.addIndex(1,3);
+        doubleList.display();
+        boolean a = doubleList.contains(4);
+        System.out.println(a);
+        int data = doubleList.remove(3);
+        System.out.println(data);
+        doubleList.remove(3);
+        doubleList.display();
+        doubleList.removeAllKey(2);
+        doubleList.display();
+    }
+}
