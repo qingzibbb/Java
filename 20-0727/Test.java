93614d382868b09797febe994b0e93eea8636885
qingzibbb <981232469@qq.com>
2020-07-27 23:31:26 +0800

    1

---
+++ 20-0727/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        boolean ret = canConstruct("abbc","acccbbdef");
+    }
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        
+    }
+}
